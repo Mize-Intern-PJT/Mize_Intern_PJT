c7ab94fcc170382004c310862d066348f7cf1363
choyuyoung <you830@naver.com>
2025-01-06 18:03:19 +0900

    [init] 25.01.06 sprintboot 초기설정

---
+++ Be/mize_intern_pjt/src/main/java/com/example/mize_intern_pjt/MizeInternPjtApplication.java
@@ -0,0 +1,13 @@
+package com.example.mize_intern_pjt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MizeInternPjtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MizeInternPjtApplication.class, args);
+	}
+
+}
---
+++ Be/mize_intern_pjt/src/test/java/com/example/mize_intern_pjt/MizeInternPjtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mize_intern_pjt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MizeInternPjtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
