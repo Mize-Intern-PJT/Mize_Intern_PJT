50058883125e618f1cb5f90b492e0c625a7601c3
choyuyoung <you830@naver.com>
2025-01-16 14:11:02 +0900

    [feat]25.01.16api생성완료

---
+++ Be/mize-intern-pjt/server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const app = express();
+const port = 3001;
+
+const getData = () => {
+  const files = [
+    'cleanedCUBE.json', 
+    'cleanedHPS.json', 
+    'cleancleanedPlug.json', 
+    'cleanedPolar1.json', 
+    'cleanedPolar2.json', 
+    'cleanedSpot.json'
+  ];
+  let allData = [];
+
+  files.forEach(file => {
+    try {
+      const data = fs.readFileSync(path.join(__dirname, file), 'utf8');
+      const jsonData = JSON.parse(data);
+      allData = allData.concat(jsonData);
+    } catch (err) {
+      console.error(`Error`, err);
+    }
+  });
+
+  return allData;
+};
+
+app.get('/api/', (req, res) => {
+  const data = getData();
+  res.json(data);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
