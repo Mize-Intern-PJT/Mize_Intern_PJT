c7bb988721967eea07feb6237221d77344cb2868
choyuyoung <you830@naver.com>
2025-01-16 17:30:25 +0900

    [feat]25.01.16api수정

--- Be/mize-intern-pjt/server.js
+++ Be/mize-intern-pjt/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const app = express();
-const port = 3001;
+const port = 3002;
 
 const getData = () => {
   const files = [
@@ -28,7 +28,7 @@ const getData = () => {
   return allData;
 };
 
-app.get('/api/', (req, res) => {
+app.get('/api', (req, res) => {
   const data = getData();
   res.json(data);
 });
