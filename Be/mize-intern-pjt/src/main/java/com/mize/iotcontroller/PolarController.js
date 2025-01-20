0b2e595768d47079f8df2ad4277d0577e1a771c0
choyuyoung <you830@naver.com>
2025-01-20 12:57:28 +0900

    [feat]25.01.20시간수정

--- Be/mize-intern-pjt/src/main/java/com/mize/iotcontroller/PolarController.js
+++ Be/mize-intern-pjt/src/main/java/com/mize/iotcontroller/PolarController.js
@@ -11,24 +11,25 @@ const APP_KEY = "CWNu6tF1jpZ1eD9s36IA6A";
 const CALLBACK_URL = "http://localhost:8081/auth_callback";
 const APP_TOKEN = "wOxwHxsdwHODCoDVFxPZog";
 
-function calculateSign(appkey, callbackurl, apptoken) {
-    const tick = Math.floor(Date.now() / 1000);
+function calculateSign(appkey, callbackurl, apptoken, tick) {
+    
     let sdata = `appkey=${appkey}&auth_callback=${callbackurl}&time=${tick}&apptoken=${apptoken}`;
     return crypto.createHash('md5').update(sdata, 'utf-8').digest('hex');
 }
 
 app.post('/controller', async (req, res) => {
   const apiUrl = 'https://api.us.ilifesmart.com/app/api.EpSet'; // 실제 IoT 기기 API URL
+  const tick = Math.floor(Date.now() / 1000);
   const requestData = {
       id: "101",
       method: "EpSet",
       system: {
           ver: "1.0",
           lang: "en",
-          sign: calculateSign(APP_KEY, CALLBACK_URL, APP_TOKEN),
+          sign: calculateSign(APP_KEY, CALLBACK_URL, APP_TOKEN, tick),
           userid: "8390501",
           appkey: "CWNu6tF1jpZ1eD9s36IA6A",
-          time: new Date().toISOString()
+          time: tick
       },
       params: {
           agt: req.body.agt,
