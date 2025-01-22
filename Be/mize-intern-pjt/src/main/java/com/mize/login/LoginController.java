89055f982b906c22ca9f5c6b4ae174ea45edee30
choyuyoung <you830@naver.com>
2025-01-22 13:07:13 +0900

    [feat]25.01.22로그인

--- Be/mize-intern-pjt/src/main/java/com/mize/login/LoginController.java
+++ Be/mize-intern-pjt/src/main/java/com/mize/login/LoginController.java
@@ -2,6 +2,9 @@ package com.mize.login;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -21,5 +24,11 @@ public class LoginController {
         } else {
             return "failed";
         }
+    }
+
+    @RequestMapping(value = "/user", method = RequestMethod.GET)
+    public String requestMethodName(@RequestParam String param) {
+        // 여기서 param을 사용하여 작업을 수행합니다
+        return "Received param: " + param;
     }
 }
