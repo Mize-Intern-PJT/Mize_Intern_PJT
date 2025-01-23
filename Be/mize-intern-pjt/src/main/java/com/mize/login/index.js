61bd7a101d2f9ee1f1b7efc72e952dad39a65991
jiwon yu <yjw5602@naver.com>
2025-01-23 16:16:46 +0900

    [feat] 25.01.23 https 추가

--- Be/mize-intern-pjt/src/main/java/com/mize/login/index.js
+++ Be/mize-intern-pjt/src/main/java/com/mize/login/index.js
@@ -1,8 +1,14 @@
-require('dotenv').config(); // .env 파일의 변수 로드
-const express = require('express');
-const oracledb = require('oracledb');
-
+require("dotenv").config(); // .env 파일의 변수 로드
+const express = require("express");
+const oracledb = require("oracledb");
+const https = require("https");
+const fs = require("fs");
+const path = require("path");
+const cors = require("cors");
 const app = express();
+
+app.use(cors());
+
 const port = process.env.PORT || 3005;
 
 // 미들웨어 설정
@@ -12,7 +18,7 @@ app.use(express.json());
 const dbConfig = {
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  connectString: process.env.DB_CONNECTION_STRING
+  connectString: process.env.DB_CONNECTION_STRING,
 };
 
 // 사용자를 인증하는 함수
@@ -26,7 +32,7 @@ async function authenticate(userId, password) {
       `SELECT * FROM users WHERE userid = :userid AND password = :password`,
       {
         userid: userId,
-        password: password
+        password: password,
       }
     );
 
@@ -46,18 +52,34 @@ async function authenticate(userId, password) {
 }
 
 // 로그인 엔드포인트
-app.post('/login', async (req, res) => {
+app.post("/login", async (req, res) => {
   const { userId, password } = req.body;
 
   const isAuthenticated = await authenticate(userId, password);
 
   if (isAuthenticated) {
-    res.status(200).json({ message: 'success' });
+    res.status(200).json({ message: "success" });
   } else {
-    res.status(401).json({ message: 'failed' });
+    res.status(401).json({ message: "failed" });
   }
 });
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+const httpsOptions = {
+  key: fs.readFileSync(
+    path.join(__dirname, "../../../../../../localhost+3-key.pem") // 정확한 경로로 수정
+  ),
+  cert: fs.readFileSync(
+    path.join(__dirname, "../../../../../../localhost+3.pem") // 정확한 경로로 수정
+  ),
+};
+
+// app.listen(port, () => {
+//   console.log(`Server running on port ${port}/login`);
+// });
+
+const PORT = process.env.PORT || 3005;
+https.createServer(httpsOptions, app).listen(PORT, "0.0.0.0", () => {
+  console.log(
+    `HTTPS API endpoint available at https://192.168.0.xx:${PORT}/login`
+  );
 });
